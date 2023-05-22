cb0c22201731cd794cfd824f42a9ccc5bb269c32
AhmadAlzo <124163736+AhmadAlzo@users.noreply.github.com>
2023-05-22 04:35:21 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import { KeyboardAvoidingView } from "react-native";
+// import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+// import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MapScreen from "./screens/MapScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Platform } from "react-native";
+// Set up Redux
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    // <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView 
+          style={{flex: 1}}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    // </Provider>
+  );
+}
